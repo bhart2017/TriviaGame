3deeb7e301b78af5be0720b76e7956f14d0be4e3
Ben Hart <benjamin_hart2006@yahoo.com>
2017-06-24 08:57:34 -0400

    Add files via upload

---
+++ TriviaGame/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Which one of these toys made its debut in the 80s?",
+	"option1": "G.I. Joe",
+	"option2": "Troll Doll",
+	"option3": "Strawberry Shortcake",
+	"option4": "Processor",
+	"answer": "Strawberry Shortcake"
+}, {
+	"question": "During the 80s, everyone wanted a pair of Ray-Ban sunglasses. What film helped to start this trend?",
+	"option1": "Top Gun",
+	"option2": "Lethal Weapon",
+	"option3": "The Godfather",
+	"option4": "Dirty Harry",
+	"answer": "Top Gun"
+}, {
+	"question": "Who played the iconic character J.R. on the TV show Dallas?",
+	"option1": "Patrick Duffy",
+	"option2": "Howard Keel",
+	"option3": "Larry Hagman",
+	"option4": "Jim Davis",
+	"answer": "Larry Hagman"
+}, {
+	"question": "In the 80s, Nintendo released their NES or Nintendo Entertainment System. What game came with the system?",
+	"option1": "Breakout",
+	"option2": "Super Mario Bros.",
+	"option3": "Frogger",
+	"option4": "Tetris",
+	"answer": "Super Mario Bros."
+}, {
+	"question": "What men's hairstyle was trendy in the 80s?",
+	"option1": "The Mullet",
+	"option2": "The Crop",
+	"option3": "Mop Top",
+	"option4": "The flip",
+	"answer": "The Mullet"
+}, {
+	"question": "What was the highest grossing movie of the 1980s?",
+	"option1": " Kramer Vs. Kramer",
+	"option2": "E.T.",
+	"option3": "Jaws",
+	"option4": "Rocky",
+	"answer": "E.T."
+}, {
+	"question": "In the 80s, Bruce Willis played which of these characters?",
+	"option1": "John Hancock",
+	"option2": "John McClane",
+	"option3": "Martin Riggs",
+	"option4": "Robert Neville",
+	"answer": "John McClane"
+}, {
+	"question": "Which of these video games was not introduced in the 1980s?",
+	"option1": "Frogger",
+	"option2": "Digger",
+	"option3": "Snake",
+	"option4": "Pac-Man",
+	"answer": "Snake"
+}, {
+	"question": "Care Bears were all the rage during the 80s. Which Care Bear was green?",
+	"option1": "Best Friend Bear",
+	"option2": "Good Luck Bear",
+	"option3": "Funshine Bear",
+	"option4": "Birthday Bear",
+	"answer": "Good Luck Bear"
+}, {
+	"question": "What 80s fashion trend was inspired by the movie Flashdance?",
+	"option1": "Bell Bottoms",
+	"option2": "Crocs",
+	"option3": "Legwarmers",
+	"option4": "Ear Muffs",
+	"answer": "Legwarmers"
+}];
